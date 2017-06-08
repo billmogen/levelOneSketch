f64a6db226337e6bddc115e77aaae77b4fa0e455
billmogen <billmogen@126.com>
2017-06-08 18:44:09 +0800

    Billmogen:work:v0.9

--- levelOneSketch.ino
+++ levelOneSketch.ino
@@ -1,5 +1,7 @@
 //levelOneSketch.ino
 
+#include <SPI.h>
+#include <RFID.h>
 // #define TUBETWODAT     5//A4
 // #define TUBETWOCLK     6//A5
 #define TUBETWODAT     A4
@@ -18,12 +20,22 @@
 
 #define NVCPIN       4   
 
-#define TEMPERATUREPIN 2
+#define TEMPERATUREPIN A6
 
+#define FANPIN      A3
+#define BUTTONPIN   2
+#define RECORDPIN   A2 
 
+#define INFRAREDPIN    A7  //红外检测 
+
+#define TRIGPIN      7
+#define ECHOPIN      8
+
+//D10 - 读卡器CS引脚、D5 - 读卡器RST引脚
+RFID rfid(10,0); 
 
 uint8_t tubeList[]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,0xbf,0xff};
-uint8_t ledList[] ={0xff,0xfe,0xfd,0xfb,0xf7,0xef,0xdf,0xbf,0x7f,0x00}; //0 - 8
+uint8_t ledList[] ={0xff,0xfe,0xfc,0xf8,0xf0,0xe0,0xc0,0x80,0x00}; //0 - 8
 uint16_t temperatureList[] = {
 
         951/*-10*/, 947/*-9*/, 943/*-8*/, 939/*-7*/, 934/*-6*/, 
@@ -51,32 +63,107 @@ uint16_t temperatureList[] = {
         140/*100*/
 
 };
+
+//the max user num for store rfid card
+unsigned long userId[50];
+uint8_t str[16];
+uint8_t  cardCount = 1;
+
 unsigned long nowTime;
 unsigned long ledOpenTime;
-uint8_t ledOpenFlag = false;
-uint8_t talkFlag = false;
+bool ledOpenFlag = false;
+bool talkFlag = false;
+bool distanceFlag = false;
+uint8_t filterFlag;
+uint16_t filterBuffer[10];
+uint16_t filterSum;
+uint16_t flowCount;
+
+int m, n; //按键状态机
 
 void setup()
 {
 	Serial.begin(9600);
 	portInit();
 	analogReference(DEFAULT);
+	SPI.begin();
+  	rfid.init();
 	attachInterrupt(1, freshOut, RISING);
+	userId[0] = 0;
+	flowCount = 0;
+	filterFlag = 0;
+	m = 0;
+	clearTube2();
+	writeLedSeven(0);
 }
 
 void loop()
 {  
 	nowTime = millis();
+	if(!digitalRead(BUTTONPIN)){
+	    delay(10);
+	    if(!digitalRead(BUTTONPIN)){
+	        m++;
+	        n = m%4;
+	        showNum2(n, 1);
+	    }
+	}
+	if(n == 3){
+	    fastTrainer();
+	}
 	if(ledOpenFlag){
-		writeLedSeven(9);
+		openFan();
+		writeLedSeven(8);
 		if(nowTime - ledOpenTime > 5000){
 		    writeLedSeven(0);
+		    closeFan();
 		    ledOpenFlag = false;
-		}   
+		} 
 	}
 	if(talkFlag){
 	    talkTemperature();
 	    talkFlag = false;
+	}
+
+	if(rfid.findCard(PICC_REQIDL, str) == MI_OK){ // 寻卡
+		//防冲突检测,读取卡序列号
+	 	if (rfid.anticoll(str) == MI_OK){
+	      
+	    	Serial.print("The card's number is  : ");
+	        //显示卡序列号
+		    for(int i = 0; i < 4; i++){
+		        Serial.print(0x0F & (str[i] >> 4),HEX);
+		        Serial.print(0x0F & str[i],HEX);
+		    }
+		    Serial.println("");
+		    memcpy(rfid.serNum,str,5);
+		    //register card id to system
+	        unsigned long tempSerNum;
+	        tempSerNum = (((unsigned long)str[0] << 24) + ((unsigned long)str[1] << 16) + ((unsigned long)str[2] << 8) + str[3]);
+
+	          //选卡，返回卡容量（锁定卡片，防止多次读写）
+		    rfid.selectTag(rfid.serNum);
+		    checkCardNumber(tempSerNum);
+		    rfid.halt();
+
+	    }  
+	}
+	if(analogRead(INFRAREDPIN) < 10){
+	    delay(5);
+	    if(analogRead(INFRAREDPIN) < 10){
+	        while(analogRead(INFRAREDPIN) < 10){
+	            // statement
+	        }
+	        flowCount++;
+	        showNum2(flowCount,2);
+	        openRecord();
+	    }
+	}
+
+	if(n == 2){
+	    uint16_t tDistance = filter(getDistance());
+	    talkNumNVC(tDistance, 1);
+	    delay(500);
 	}
 
 }
@@ -103,16 +190,100 @@ void portInit() {
 
 	pinMode(TEMPERATUREPIN, INPUT_PULLUP);
 
+	pinMode(FANPIN, OUTPUT);
+	digitalWrite(FANPIN, LOW);
+	pinMode(BUTTONPIN, INPUT_PULLUP);
+
+	pinMode(TRIGPIN, OUTPUT);
+	pinMode(ECHOPIN, INPUT);
+
+	pinMode(RECORDPIN, OUTPUT);
+	digitalWrite(RECORDPIN, LOW);
 }
 
+void fastTrainer() {
+	for(int i=0; i<3; i++){
+	    writeLedSeven(8);
+	    delay(1000);
+	    writeLedSeven(0);
+	    delay(1000);
+	}
+	uint16_t tmp = random(1000, 5000);
+	delay(tmp);
+	writeLedSeven(8);
+	delay(50);
+	writeLedSeven(0);
+	for(int i=1; i<9; i++){
+	    writeLedSeven(i);
+	    delay(50);
+	    if(!digitalRead(BUTTONPIN)){
+	        delay(5);
+	        if(!digitalRead(BUTTONPIN)){
+	            break;
+	        }
+	    }
+	}
+	delay(5000);
+	writeLedSeven(0);
+	n = 0;
+}
+bool checkCardNumber(unsigned long sernum) {
+
+	uint8_t cardId = 0;
+	uint8_t i;
+	//遍历搜索
+	for(i = 0; i < cardCount; i++)
+    {
+
+        if(userId[i] == sernum)
+        {
+            cardId = i;
+            showNum2(cardId, 2);
+            openBuzzer();
+            delay(500);
+            closeBuzzer();
+            //run the elec fan
+            uint8_t stat = digitalRead(FANPIN);
+            stat ^= 1;
+            digitalWrite(FANPIN, stat);
+            return true;
+        }
+
+    }
+    //注册卡号
+    if(i == cardCount && n == 1)
+    {
+        userId[i - 1] = sernum;
+        userId[i] = 0;
+        cardId = i - 1;
+        //call beep 
+        openBuzzer();
+        delay(500);
+        closeBuzzer();
+        //show card id that stored
+        //Serial.println(cardId);
+        showNum2(cardId, 2);
+        if(cardCount == 50)
+        {
+            cardCount = 1;
+        }else {
+            cardCount++;
+        }
+        return true;
+        
+    }else {
+    	errorTube2();
+    	return false;
+    }
+    
+} 
 void freshOut() {
 	if(digitalRead(3)){
-		delay(1);
+		delay(10);
 		if(digitalRead(3)){
        		talkFlag = true;
        		ledOpenFlag = true;
        		ledOpenTime = millis();
-
 		}
 	}
 }
@@ -138,13 +309,12 @@ void talkTemperature() {
         errorNVC();
         Serial.print(" error");
     } else {
-    	talkNumNVC(tempVal); 
-    	delay(1000);
+    	talkNumNVC(tempVal, 0); 
     }
     Serial.println();
 }
 void errorNVC() {
-	writeNVC(23);
+	writeNVC(0x0c);
 }
 void writeNVC(uint8_t num) {
 	unsigned char number = num;
@@ -173,49 +343,110 @@ void writeNVC(uint8_t num) {
     }
     digitalWrite(NVCPIN, HIGH); 
 }
+/*
+@num: 0-999
+@type: 0:temperature 1: 距离
+ */
+bool talkNumNVC(uint16_t num, uint8_t type) {
+	uint8_t gewei = num%10;
+    uint8_t shiwei = num%100/10;
+    uint8_t baiwei = num/100;
+    Serial.print("baiwei: ");
+    Serial.print(baiwei);
+    Serial.print(" shiwei: ");
+    Serial.print(shiwei);
+    Serial.print(" gewei: ");
+    Serial.println(gewei);
+    if(num > 999){
+        errorNVC();
+        return false;
+    }
 
-void talkNumNVC(uint8_t num) {
-	unsigned char gewei = num%10;
-    unsigned char shiwei = num/10;
-    delay(100);
-    writeNVC(0x0c); //当前
-    delay(450);
-    writeNVC(0x0d);//温度
-    delay(1000);
+    if(type == 0){
+		writeNVC(0x0c); //当前
+	    delay(450);
+	    writeNVC(0x0d);//温度
+	    delay(1000);        
+    } else {
+
+    }
     if(num < 10)
     {
         writeNVC(gewei);
-        delay(500);
-    }
-    else if(num == 10)
-    {   
+        delay(300);
+    } else if(num == 10) {   
+        writeNVC(0x0a);  //拾
+        delay(300);
+    } else if(num > 10 && num < 20) {
         writeNVC(0x0a);
-        delay(500);
-    }
-    else if(num > 10 && num < 20)
-    {
-        writeNVC(0x0a);
-        delay(500);
+        delay(400);
         writeNVC(gewei);
-        delay(500);
+        delay(300);
+    } else if(num >= 20){
+    	
+    	if(baiwei != 0){
+        writeNVC(baiwei);
+        delay(300);
+        writeNVC(0x0b);
+        delay(300);
+	    } 
+	    if(shiwei != 0){
+	        writeNVC(shiwei);
+	        delay(500);
+	        writeNVC(0x0a);
+	        delay(500);
+	        if(gewei != 0){
+	            writeNVC(gewei);
+	            delay(300);
+	        }
+	    } else {
+	    	if(gewei != 0){
+	    		writeNVC(0x00);
+	    		delay(300);
+	    		writeNVC(gewei);   
+	    	}
+	    }    
     }
-    else if(gewei == 0)
-    {
-        writeNVC(shiwei);
-        delay(500);
-        writeNVC(0x0a);
-        delay(500);
+
+    if(type == 0){
+    	writeNVC(0x0e); //摄氏度    
     }
-    else
-    {
-        writeNVC(shiwei);
-        delay(500);
-        writeNVC(0x0a); //拾
-        delay(500);
-        writeNVC(gewei);
-        delay(500);
-    }   
-    writeNVC(0x0e); //摄氏度
+    return true;
+    
+    // if(num < 10)
+    // {
+    //     writeNVC(gewei);
+    //     delay(500);
+    // }
+    // else if(num == 10)
+    // {   
+    //     writeNVC(0x0a);  //拾
+    //     delay(500);
+    // }
+    // else if(num > 10 && num < 20)
+    // {
+    //     writeNVC(0x0a);
+    //     delay(500);
+    //     writeNVC(gewei);
+    //     delay(500);
+    // }
+    // else if(gewei == 0)
+    // {
+    //     writeNVC(shiwei);
+    //     delay(500);
+    //     writeNVC(0x0a);
+    //     delay(500);
+    // }
+    // else
+    // {
+    //     writeNVC(shiwei);
+    //     delay(500);
+    //     writeNVC(0x0a); //拾
+    //     delay(500);
+    //     writeNVC(gewei);
+    //     delay(500);
+    // }  
+    
 }
 /*
 function: open the LED 74hc164
@@ -278,6 +509,54 @@ void openBuzzer() {
 void closeBuzzer() {
 	digitalWrite(BUZZERPIN, LOW);
 }
+void openFan() {
+	digitalWrite(FANPIN, HIGH);
+}
+void closeFan() {
+	digitalWrite(FANPIN, LOW);
+}
+void openRecord() {
+	digitalWrite(RECORDPIN, HIGH);
+	delay(20);
+	digitalWrite(RECORDPIN, LOW);
+}
 
+uint16_t getDistance() {
+	float distance = 0;
+	digitalWrite(TRIGPIN, HIGH);
+	delayMicroseconds(20);
+	digitalWrite(TRIGPIN, LOW);
+	distance = pulseIn(ECHOPIN, HIGH, 100000)/58.00;
+	// Serial.println(distance);
+	return (uint16_t)distance;
+}
 
+uint16_t filter(uint16_t m) {
+	const int buffMax = 10;
+	int i;
+	Serial.print("m: ");
+	Serial.print(m);
+	if(filterFlag == 0)
+	{
+		filterFlag = 1;
 
+		for(i = 0, filterSum = 0; i < buffMax; i++)
+		{
+		  filterBuffer[i] = m;
+		  filterSum += filterBuffer[i];
+		}
+		return m;
+	} else {
+		filterSum -= filterBuffer[0];
+		for(i = 0; i < (buffMax - 1); i++)
+		{
+		  filterBuffer[i] = filterBuffer[i + 1];
+		}
+		filterBuffer[9] = m;
+		filterSum += filterBuffer[9];
+		Serial.print(" Sum: ");
+		Serial.println(filterSum);
+		return  (filterSum / 10);
+	  
+	}
+}
